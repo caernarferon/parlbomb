b8f4f93d54298681b68837bce7f21ecd2a586d21
parliamentbomber <109875196+caernarferon@users.noreply.github.com>
2024-09-02 20:10:02 -0600

    sigma
    Signed-off-by: parliamentbomber <109875196+caernarferon@users.noreply.github.com>

--- system/programs/syncthing.nix
+++ system/programs/syncthing.nix
@@ -9,20 +9,13 @@
       overrideFolders = true; # overrides any folders added or deleted through the WebUI
       settings = {
         devices = {
-          "digglydoo" = {id = "DEVICE-ID-GOES-HERE";};
+          "digglydoo" = {id = "XYZH56F-4UJZZDE-H5XT3DW-TGZ35Z2-6P237WU-D2A7A4Y-2HAGVEE-PCE5NQ4";};
           "pessima" = {id = "UD43H26-MEFFACM-UKIQBNC-VYYBEGP-XHQ2T2S-EPYCNTF-IT4NNJW-SJGXBQD";};
         };
         folders = {
           "Documents" = {
-            # Folder ID in Syncthing, also the name of folder (label) by default
-            path = "/home/myusername/Documents"; # Which folder to add to Syncthing
-            devices = ["device1" "device2"]; # Which devices to share the folder with
-          };
-          "Example" = {
-            label = "Private"; # Optional label for the folder
-            path = "/home/myusername/Example";
-            devices = ["device1"];
-            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+            path = "/home/parliamentbomber/Documents";
+            devices = ["digglydoo" "pessima"];
           };
         };
       };
